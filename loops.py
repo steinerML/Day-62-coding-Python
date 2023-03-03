bf34450398d7d25bbae3e7059f864c775e140739
steinerML <102678863+steinerML@users.noreply.github.com>
2023-03-03 18:19:12 +0100

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,19 @@
+#Printing a list with a loop
+nums = [10,20,30,40,55]
+for num in nums:
+    print(num)
+
+#Printing a dictionary with a loop
+numbs = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five'}
+for numb in numbs:
+    print(numb)
+
+#Printing a string with a loop
+char = 'Politicians suck'
+for ch in char:
+    print(ch)
+
+#Printing a tuple with a loop
+nums = (1,3,44,55,63,99)
+for num in nums:
+    print(num)
